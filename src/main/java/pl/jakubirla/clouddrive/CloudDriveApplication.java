39d41c6c046621bec0b9fa11c0c73689390d9394
Jakub Irla <irlajakub@gmail.com>
2024-04-29 16:27:20 +0200

    Instalacja spring boot, tworzenie controllerów, spring security - konfiguracja

---
+++ src/main/java/pl/jakubirla/clouddrive/CloudDriveApplication.java
@@ -0,0 +1,16 @@
+package pl.jakubirla.clouddrive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class CloudDriveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudDriveApplication.class, args);
+    }
+}
---
+++ src/main/java/pl/jakubirla/clouddrive/HelloController.java
@@ -0,0 +1,26 @@
+package pl.jakubirla.clouddrive;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @GetMapping("")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hello %s from HelloController", name);
+    }
+
+    @GetMapping("user")
+    public String helloUser() {
+        return "Hello User";
+    }
+
+    @GetMapping("admin")
+    public String helloAdmin() {
+        return "Hello Admin";
+    }
+}
